9ce96fe424a55e505be3b6706f83c191b9efa588
Brandon Liu <bdon@bdon.org>
2015-09-04 22:53:43 +0800

    add tasking

---
+++ create_tasks.py
@@ -0,0 +1,36 @@
+# start with a full building database.
+# find the extent of the dataset
+
+import psycopg2
+import psycopg2.extras
+import json
+from shapely.wkb import loads
+from shapely.geometry import box, mapping
+
+conn = psycopg2.connect(dbname="tpe_buildings")
+cur = conn.cursor()
+
+def fname(tup):
+  return "%03dX_%03dY" % tup
+
+current_task = None
+current_buildings = []
+cur.execute("select col, row, id, ST_AsBinary(geom), height from buildings order by (col, row) asc")
+for building in cur:
+  task = (building[0],building[1])
+  if current_task and current_task != task:
+    with open('tasks/' + fname(current_task) + '.geojson','w') as f:
+      f.write(json.dumps({'type':'FeatureCollection','features':current_buildings}))
+    current_buildings = []
+  current_task = task
+  current_buildings.append({
+    'type':'Feature',
+    'properties':{'height':building[4]},
+    'id':building[2],
+    'geometry':mapping(loads(str(building[3])))
+  })
+
+
+
+
+
--- setup.sql
+++ setup.sql
@@ -3,5 +3,8 @@ CREATE EXTENSION hstore;
 
 DROP TABLE buildings;
 
-CREATE TABLE buildings(id int, height float);
+CREATE TABLE buildings(id int, height float, row int, col int);
 SELECT AddGeometryColumn('buildings', 'geom', 4326, 'Geometry', 2);
+CREATE INDEX buildings_idx ON buildings USING GIST (geom);
+CREATE INDEX buildings_row_idx on buildings(row);
+CREATE INDEX buildings_col_idx on buildings(col);
